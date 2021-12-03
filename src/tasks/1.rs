bdc9914bd799dbd346701bda5a9c91d7f5704b2d
Wojtek <woj.lass@protonmail.com>
2021-12-04 00:39:54 +0100

    reformating + added testing

--- src/tasks/1.rs
+++ src/tasks/1.rs
@@ -1,83 +1,115 @@
-
-use std::fs;
 use std::collections::VecDeque;
+use std::fs;
+use std::str::SplitWhitespace;
 
-struct FTask{
-   prev : i32,
-   counter: i32
+struct FTask {
+    prev: i32,
+    counter: i32,
 }
 
 impl FTask {
-   fn new(s_prev: &str) -> FTask {
-      FTask { prev: s_prev.parse::<i32>().unwrap(), counter: 0 }
-  }
+    fn new(s_prev: &str) -> FTask {
+        FTask {
+            prev: s_prev.parse::<i32>().unwrap(),
+            counter: 0,
+        }
+    }
 
-   fn count(&mut self, now :&str ){
-      let my_int = now.parse::<i32>().unwrap();
-      if my_int >self.prev {
-         self.counter+=1;
-
-      }
-      self.prev = my_int;
-
-   }
+    fn count(&mut self, now: &str) {
+        let my_int = now.parse::<i32>().unwrap();
+        if my_int > self.prev {
+            self.counter += 1;
+        }
+        self.prev = my_int;
+    }
 }
 
-struct STask{
-   prev : VecDeque<i32>,
-   prevsum: i32,
-   counter: i32
+struct STask {
+    prev: VecDeque<i32>,
+    prevsum: i32,
+    counter: i32,
 }
 
 impl STask {
-   fn new(prev1: &str, prev2: &str, prev3: &str,) -> STask {
-      let deque = VecDeque::from([prev1.parse::<i32>().unwrap(), 
-                        prev2.parse::<i32>().unwrap(), 
-                        prev3.parse::<i32>().unwrap()]
-                        .to_vec());
-      let sum: i32 = deque.iter().sum();
+    fn new(prev1: &str, prev2: &str, prev3: &str) -> STask {
+        let deque = VecDeque::from(
+            [
+                prev1.parse::<i32>().unwrap(),
+                prev2.parse::<i32>().unwrap(),
+                prev3.parse::<i32>().unwrap(),
+            ]
+            .to_vec(),
+        );
+        let sum: i32 = deque.iter().sum();
 
-      STask { 
-         prev: VecDeque::from(deque),
-         prevsum: sum,
-         counter: 0, 
-      }
-  }
+        STask {
+            prev: VecDeque::from(deque),
+            prevsum: sum,
+            counter: 0,
+        }
+    }
 
-   fn count(&mut self, now :&str ){
-      let my_int = now.parse::<i32>().unwrap();
-      self.prev.pop_front();
-      self.prev.push_back(my_int);
-      let sum = self.prev.iter().sum();
+    fn count(&mut self, now: &str) {
+        let my_int = now.parse::<i32>().unwrap();
+        self.prev.pop_front();
+        self.prev.push_back(my_int);
+        let sum = self.prev.iter().sum();
 
-      if sum >self.prevsum {
-         self.counter+=1;
-
-      }
-      self.prevsum = sum;
-
-   }
+        if sum > self.prevsum {
+            self.counter += 1;
+        }
+        self.prevsum = sum;
+    }
 }
 
+fn task1(mut split: SplitWhitespace) -> i32 {
+    let i1 = split.next().unwrap();
+    let mut f = FTask::new(&i1);
+    for i in split {
+        f.count(&i);
+    }
+    f.counter
+}
+
+fn task2(mut split: SplitWhitespace) -> i32 {
+    let i1 = split.next().unwrap();
+    let i2 = split.next().unwrap();
+    let i3 = split.next().unwrap();
+    let mut f2 = STask::new(&i1, &i2, &i3);
+    for i in split {
+        f2.count(&i);
+    }
+    f2.counter
+}
 
 fn main() {
-    let contents = fs::read_to_string("resources/1.txt")
-        .expect("Something went wrong reading the file");
-         let mut split = contents.split_whitespace();
+    let contents =
+        fs::read_to_string("resources/1.txt").expect("Something went wrong reading the file");
+    let split = contents.split_whitespace();
+    let split2 = split.clone();
+    print!(
+        "count 1 :\n{}\ncount 2 :\n{}\n",
+        task1(split),
+        task2(split2)
+    );
+}
 
-         let i1 = split.next().unwrap();
-         let i2 = split.next().unwrap();
-         let i3 = split.next().unwrap();
+#[cfg(test)]
+mod test {
+    use super::*;
+    #[test]
+    fn test_part1() {
+        let contents =
+            fs::read_to_string("resources/1t.txt").expect("Something went wrong reading the file");
+        let split = contents.split_whitespace();
+        assert_eq!(7, task1(split));
+    }
 
-         let mut f = FTask::new(&i1);
-         f.count(&i2);
-         f.count(&i3);
-
-         let mut f2 = STask::new(&i1, &i2, &i3);
-         
-         for  i in split{
-            f.count(&i);
-            f2.count(&i);
-         }
-         print!("count 1 :\n{}\ncount 2 :\n{}\n", f.counter, f2.counter);
+    #[test]
+    fn test_part2() {
+        let contents =
+            fs::read_to_string("resources/1t.txt").expect("Something went wrong reading the file");
+        let split = contents.split_whitespace();
+        assert_eq!(5, task2(split));
+    }
 }
--- src/tasks/2.rs
+++ src/tasks/2.rs
@@ -1,92 +1,121 @@
+use std::{fs, str::Split};
 
-use std::fs;
-use std::collections::VecDeque;
+trait Task {
+    fn count(&mut self, direction: &str, value: &str);
 
-struct FTask{
-    horisontal : i32,
-    depth: i32
- }
- 
- impl FTask {
-    fn new() -> FTask {
-       FTask { horisontal: 0, depth: 0 }
-   }
- 
-    fn count(&mut self, direction :&str , value :&str ){
-       let int_val = value.parse::<i32>().unwrap();
- 
-         let s = String::from(direction);
-       match s.as_str() {
-         "forward" => {
-             self.horisontal+=int_val;
-          },
-          "up" => {
-             self.depth-=int_val;
-           },
-           "down" => {
-             self.depth+=int_val;
-            },
-          _ => {
-              println!("No");
-          }
-     }
-       
- 
+    fn run(&mut self, split: Split<&str>) {
+        for i in split {
+            let mut spi = i.split_whitespace();
+            let dir = spi.next().unwrap();
+            let val = spi.next().unwrap();
+            self.count(&dir, &val);
+        }
     }
- }
- 
- struct STask{
-    horisontal : i32,
+}
+
+struct FTask {
+    horisontal: i32,
     depth: i32,
-    aim: i32
- }
- 
- impl STask {
-    fn new() -> STask {
-        STask { horisontal: 0, depth: 0,  aim: 0}
-   }
- 
-    fn count(&mut self, direction :&str , value :&str ){
-       let int_val = value.parse::<i32>().unwrap();
- 
-         let s = String::from(direction);
-       match s.as_str() {
-         "forward" => {
-            self.horisontal+=int_val;
-            self.depth+=int_val*self.aim;
-          },
-          "up" => {
-            self.aim-=int_val;
-           },
-           "down" => {
-            self.aim+=int_val;
-            },
-          _ => {
-              println!("No");
-          }
-     }
-       
- 
+}
+
+impl Task for FTask {
+    fn count(&mut self, direction: &str, value: &str) {
+        let int_val = value.parse::<i32>().unwrap();
+
+        let s = String::from(direction);
+        match s.as_str() {
+            "forward" => {
+                self.horisontal += int_val;
+            }
+            "up" => {
+                self.depth -= int_val;
+            }
+            "down" => {
+                self.depth += int_val;
+            }
+            _ => {
+                println!("No");
+            }
+        }
     }
- }
- 
-  
+}
+impl FTask {
+    fn new() -> FTask {
+        FTask {
+            horisontal: 0,
+            depth: 0,
+        }
+    }
+}
+
+struct STask {
+    horisontal: i32,
+    depth: i32,
+    aim: i32,
+}
+
+impl STask {
+    fn new() -> STask {
+        STask {
+            horisontal: 0,
+            depth: 0,
+            aim: 0,
+        }
+    }
+}
+impl Task for STask {
+    fn count(&mut self, direction: &str, value: &str) {
+        let int_val = value.parse::<i32>().unwrap();
+
+        let s = String::from(direction);
+        match s.as_str() {
+            "forward" => {
+                self.horisontal += int_val;
+                self.depth += int_val * self.aim;
+            }
+            "up" => self.aim -= int_val,
+            "down" => self.aim += int_val,
+            _ => {
+                println!("No");
+            }
+        }
+    }
+}
 fn main() {
-    let contents = fs::read_to_string("resources/2.txt")
-        .expect("Something went wrong reading the file");
-         let mut split = contents.split("\n");
+    let contents =
+        fs::read_to_string("resources/2.txt").expect("Something went wrong reading the file");
+    let split = contents.split("\n");
+    let split2 = split.clone();
 
+    let mut f = FTask::new();
+    f.run(split);
+    print!("count 1 :\n{}\n", f.horisontal * f.depth);
+    let mut f2 = STask::new();
+    f2.run(split2);
+
+    print!("count 2 :\n{}\n", f2.horisontal * f2.depth);
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    #[test]
+    fn test_part1() {
+        let contents =
+            fs::read_to_string("resources/2t.txt").expect("Something went wrong reading the file");
+        let split = contents.split("\n");
         let mut f = FTask::new();
-        let mut f2 = STask::new();
-         
-         for  i in split{
-             let mut spi = i.split_whitespace();
-             let dir = spi.next().unwrap();
-             let val = spi.next().unwrap();
+        f.run(split);
+        assert_eq!(150, f.horisontal * f.depth);
+    }
 
-             f.count(&dir,&val);
-             f2.count(&dir,&val);
-         }
-         print!("count 1 :\n{}\n", f.horisontal*f.depth);
-         print!("count 2 :\n{}\n", f2.horisontal*f2.depth);
+    #[test]
+    fn test_part2() {
+        let contents =
+            fs::read_to_string("resources/2t.txt").expect("Something went wrong reading the file");
+        let split = contents.split("\n");
+        let mut f = STask::new();
+        f.run(split);
+        assert_eq!(900, f.horisontal * f.depth);
+    }
 }
